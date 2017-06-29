d8a0ccc412449ee6c91c1cd6de0ce31b3ac6f2ce
Yan Alves <yanalves@hotmail.com>
2017-06-29 01:00:10 -0300

    :gift: Adiciona busca de personagens

--- app/src/main/java/com/example/yanal/knowyourhero/Views/yaMainActivity.java
+++ app/src/main/java/com/example/yanal/knowyourhero/Views/yaMainActivity.java
@@ -7,7 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.yanal.knowyourhero.Adapters.yaMarvelCharacterAdapter;
 import com.example.yanal.knowyourhero.Models.yaMarvelCharacter;
@@ -20,6 +22,8 @@ import java.util.List;
 public class yaMainActivity extends AppCompatActivity {
     private yaMarvelCharacterAdapter marvelCharacterAdapter;
     private ProgressDialog load;
+    private String searchPattern;
+    GetDataFromMarvelApi taskGetCharacters = new GetDataFromMarvelApi();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,17 +35,12 @@ public class yaMainActivity extends AppCompatActivity {
         lView.setAdapter(marvelCharacterAdapter);
 
         //Chamando a task async para recuperar dados da API
-        GetDataFromMarvelApi data = new GetDataFromMarvelApi();
-        data.execute();
+
+        taskGetCharacters.execute();
 
         lView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                //Ex 2 - Pegando conteudo do filho da listview
-                //TextView textView = (TextView) view.findViewById(R.id.txtLogin);
-                // Toast.makeText(MainActivity.this,"Clicou em "+ textView.getText().toString(), Toast.LENGTH_LONG).show();
-
-                //Ex 3 - Indo para uma nova view
                 Intent intent = new Intent(yaMainActivity.this, yaDetailActivity.class);
 
                 yaMarvelCharacter character = marvelCharacterAdapter.getItem(position);
@@ -52,19 +51,32 @@ public class yaMainActivity extends AppCompatActivity {
         });
     }
 
+    public void searchCharacters(View view)
+    {
+        EditText searchBox = (EditText) findViewById(R.id.txtBoxSearch);
+        searchPattern = searchBox.getText().toString();
+
+        new GetDataFromMarvelApi().execute();
+    }
+
     private class GetDataFromMarvelApi extends AsyncTask<Void, Void, List<yaMarvelCharacter>> {
 
         @Override
         protected void onPreExecute(){
-            load = ProgressDialog.show(yaMainActivity.this, "Por favor Aguarde ...", "Recuperando Informações do Servidor...");
+            load = ProgressDialog.show(yaMainActivity.this, "Por favor Aguarde ...", "Buscando informações dos personagens...");
         }
 
         @Override
         protected List<yaMarvelCharacter> doInBackground(Void... params) {
-            List<yaMarvelCharacter> loadedCharacters = new ArrayList<yaMarvelCharacter>();
+            List<yaMarvelCharacter> loadedCharacters;
             yaApiUtils util = new yaApiUtils();
 
-            loadedCharacters = util.getData("http://gateway.marvel.com/v1/public/characters?ts=1&apikey=7f4a9857bd4998b55a01eb9f09d40e90&hash=fb7392548e27da1bd6fc4909be65c829&limit=50&nameStartsWith=Sp");
+            String charactersRouteURL = "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=7f4a9857bd4998b55a01eb9f09d40e90&hash=fb7392548e27da1bd6fc4909be65c829&limit=50";
+            if (searchPattern != null && !searchPattern.trim().isEmpty())
+            {
+                charactersRouteURL += "&nameStartsWith=" + searchPattern;
+            }
+            loadedCharacters = util.getData(charactersRouteURL);
             return loadedCharacters;
         }
 
@@ -72,6 +84,7 @@ public class yaMainActivity extends AppCompatActivity {
         protected void onPostExecute(List<yaMarvelCharacter> characters){
             super.onPostExecute(characters);
             load.dismiss();
+            searchPattern = "";
             marvelCharacterAdapter.setItemList(characters);
             marvelCharacterAdapter.notifyDataSetChanged();
         }
